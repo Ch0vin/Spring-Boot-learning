d8296721ed321724712a4edd12462c9c0baa8694
Chovin <152864419+Ch0vin@users.noreply.github.com>
2024-12-17 15:52:24 +0300

    Update SwaggerConfig.java

--- springboot/src/main/java/com/springboot/config/SwaggerConfig.java
+++ springboot/src/main/java/com/springboot/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
+//create swager ui for showing all routes
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
